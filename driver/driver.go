9cc26a840570e262f14b38b8509a8595a01d8f04
wbc6080 <wangbincheng4@huawei.com>
2025-03-26 10:26:48 +0800

    fix to the v1.20
    Signed-off-by: wbc6080 <wangbincheng4@huawei.com>

--- driver/driver.go
+++ driver/driver.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 
 	"k8s.io/klog/v2"
+
+	"github.com/kubeedge/mapper-framework/pkg/common"
 )
 
 func NewClient(protocol ProtocolConfig) (*CustomizedClient, error) {
@@ -19,15 +21,15 @@ func NewClient(protocol ProtocolConfig) (*CustomizedClient, error) {
 
 func (c *CustomizedClient) InitDevice() error {
 	// TODO: add init operation
-	// you can use c.ProtocolConfig and c.ProtocolCommonConfig
-	klog.Infof("Init device%d successful, protocolID: %v", c.DeviceID, c.ProtocolID)
+	// you can use c.ProtocolConfig
+	klog.Infof("Init device %d successful, protocolID: %v", c.DeviceID, c.ProtocolID)
 	klog.Infof("I can get Info: %v %v ", c.SerialPort, c.BaudRate)
 	return nil
 }
 
 func (c *CustomizedClient) GetDeviceData(visitor *VisitorConfig) (interface{}, error) {
-	// TODO: get device's data
-	// you can use c.ProtocolConfig,c.ProtocolCommonConfig and visitor
+	// TODO: add the code to get device's data
+	// you can use c.ProtocolConfig and visitor
 	if visitor.VisitorConfigData.DataType == "int" {
 		if c.intMaxValue <= 0 {
 			return nil, fmt.Errorf("max value is %d, should > 0", c.intMaxValue)
@@ -35,15 +37,20 @@ func (c *CustomizedClient) GetDeviceData(visitor *VisitorConfig) (interface{}, e
 		return 12, nil
 	} else if visitor.DataType == "float" {
 		return rand.Float64(), nil
-	} else {
-		return nil, fmt.Errorf("unrecognized data type: %s", visitor.DataType)
 	}
-	return nil, nil
+	return nil, fmt.Errorf("unrecognized data type: %s", visitor.DataType)
+}
+
+func (c *CustomizedClient) DeviceDataWrite(visitor *VisitorConfig, deviceMethodName string, propertyName string, data interface{}) error {
+	// TODO: add the code to write device's data
+	// you can use c.ProtocolConfig and visitor to write data to device
+	klog.Infof("Write data %s to property %s", data, propertyName)
+	return nil
 }
 
 func (c *CustomizedClient) SetDeviceData(data interface{}, visitor *VisitorConfig) error {
 	// TODO: set device's data
-	// you can use c.ProtocolConfig,c.ProtocolCommonConfig and visitor
+	// you can use c.ProtocolConfig and visitor
 	if visitor.DataType == "int" {
 		c.intMaxValue = int(data.(int64))
 	} else {
@@ -54,7 +61,12 @@ func (c *CustomizedClient) SetDeviceData(data interface{}, visitor *VisitorConfi
 
 func (c *CustomizedClient) StopDevice() error {
 	// TODO: stop device
-	// you can use c.ProtocolConfig and c.ProtocolCommonConfig
-	klog.Infof("Stop device%d successful", c.DeviceID)
+	// you can use c.ProtocolConfig
+	klog.Infof("Stop device %d successful", c.DeviceID)
 	return nil
+}
+
+func (c *CustomizedClient) GetDeviceStates() (string, error) {
+	// TODO: GetDeviceStates
+	return common.DeviceStatusOK, nil
 }
